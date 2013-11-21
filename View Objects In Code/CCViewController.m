6cd4a02b070042e515bf32e64728a44f1e1b9d5a
Warren Deshazo <warrendeshazo@Warrens-iMac.local>
2013-11-20 21:58:04 -0500

    first commit

--- View Objects In Code/CCViewController.m
+++ View Objects In Code/CCViewController.m
@@ -9,6 +9,8 @@
 #import "CCViewController.h"
 
 @interface CCViewController ()
+@property (strong, nonatomic) UILabel *myLabel;
+@property (strong, nonatomic) UITextField *myTextField;
 
 @end
 
@@ -18,6 +20,34 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.myLabel = [[UILabel alloc]init];
+    self.myLabel.frame = CGRectMake (20, 60, 280, 22);
+    [self.view addSubview:self.myLabel];
+    self.myLabel.textColor = [UIColor blueColor];
+    self.myLabel.text = @"placeholder";
+    
+    self.myTextField = [[UITextField alloc]init];
+    self.myTextField.frame = CGRectMake(20, 110, 280, 30);
+    self.myTextField.placeholder = @"Placeholder text";
+    self.myTextField.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:self.myTextField];
+    
+    
+    UIButton *myButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    myButton.frame = CGRectMake(20, 160, 280, 30);
+    [myButton setTitle:@"press me damn it!" forState:UIControlStateNormal];
+    
+    [myButton addTarget:self action:@selector(didPressButton:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:myButton];
+
+}
+
+-(void)didPressButton:(UIButton *)sender;
+{
+    self.myLabel.text = self.myTextField.text;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
